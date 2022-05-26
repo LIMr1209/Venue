a4e7f75805bb82bb6c7920f87bb46a1f0589f4d8
LIMr1209 <aaa1058169464@126.com>
2022-05-26 18:00:54 +0800

    first

---
+++ Assets/Editor/CreateAssetBundles.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor;
+using UnityEngine.Windows;
+
+public class CreateAssetBundles
+{
+    [MenuItem("AB/Build AssetBundles")]
+    static void BuildAllAssetBundles()
+    {
+        //要创建的目录
+        string assetBundleDirectory = "Assets/AssetBundles";
+        if(!Directory.Exists(assetBundleDirectory))
+        {
+            Directory.CreateDirectory(assetBundleDirectory);
+        }
+        //三个参数：第一个是创建的目录位置，第二个是AssetBundle的压缩方式，第三个是创建的平台。
+        BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
+    }
+}
---
+++ Assets/Scripts/InitialScene.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+using UnityEditor;
+
+namespace DefaultNamespace
+{
+    public class InitialScene : MonoBehaviour
+    {
+        // 初始化 加载场景预制体
+        // 在Unity3D中Project视窗中创建文件夹：Resources。
+        // 将需要动态加载的文件放入其中，例如Texture，Sprite，prefab等等。
+        // 在脚本中调用API接口Resources.Load()相关接口即可。
+        // 此种方式只能访问Resources文件夹下的资源。
+        public string sceneModel = "Models/场馆";
+
+        private void Awake()
+        {
+            GameObject model = Resources.Load<GameObject>(sceneModel);
+            GameObject newModel = Instantiate(model, new Vector3(1,1,1), Quaternion.identity);
+            // var tArray = Resources.FindObjectsOfTypeAll(typeof(MeshRenderer ));
+            MeshRenderer[] meshRender = newModel.gameObject.GetComponentsInChildren<MeshRenderer>();
+            foreach (MeshRenderer t in meshRender)
+            {
+                t.gameObject.AddComponent<MeshCollider>();
+            }
+            
+            Camera[] cameras = newModel.gameObject.GetComponentsInChildren<Camera>();
+            foreach (Camera c in cameras)
+            {
+                c.enabled = false;
+            }
+        }
+    }
+}
