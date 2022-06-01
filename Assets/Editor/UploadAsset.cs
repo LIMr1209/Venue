57ae92c935ad7d5a227ce38032a96f3cc91b1131
LIMr1209 <aaa1058169464@126.com>
2022-06-01 17:12:47 +0800

    优化

---
+++ Assets/Editor/UploadAsset.cs
@@ -0,0 +1,109 @@
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using DefaultNamespace;
+using Qiniu.Http;
+using Qiniu.Storage;
+using UnityEditor;
+using Qiniu.Util;
+using FileInfo = System.IO.FileInfo;
+
+namespace Editor
+{
+    public class UploadAsset : MonoBehaviour
+    {
+        [MenuItem("Tools/上传七牛")]
+        public static void Upload()
+        {
+
+            Debug.Log(111);
+            UploadApplication();
+            // EditorCoroutine.start(UploadApplication());
+
+        }
+
+        public static void  UploadApplication()
+        {
+            List<string> files = new List<string>();
+            DirectoryInfo theFolder = new DirectoryInfo(Application.dataPath);
+            DirectoryInfo[] dirInfo = theFolder.GetDirectories();
+            foreach (DirectoryInfo NextFolder in dirInfo)
+            {
+                if (NextFolder.Name.EndsWith("AssetBundles") || NextFolder.Name.EndsWith("Resources"))
+                {
+                    ForeachFile(NextFolder.FullName, ref  files); 
+                }
+            }
+            foreach (string i  in files)
+            {
+                HttpResult result = UploadFile(i);
+                if (result.Code != 200)
+                {
+                    Debug.Log("上传错误 文件: " + i + " 错误消息: "+result.Text);
+                    return;
+                }
+            }
+        }
+
+        public static void ForeachFile(string filePathByForeach,ref List<string> files)
+
+        {
+            DirectoryInfo theFolder = new DirectoryInfo(filePathByForeach);
+            DirectoryInfo[] dirInfo = theFolder.GetDirectories();//获取所在目录的文件夹
+            FileInfo[] file=  theFolder.GetFiles();//获取所在目录的文件
+     
+            foreach (FileInfo fileItem in file) //遍历文件
+            {
+                if (!FilterFile(fileItem))
+                {
+                    string result = Path.Combine(fileItem.DirectoryName, fileItem.Name);
+                    files.Add(result);
+                }
+            }
+            //遍历文件夹
+            foreach (DirectoryInfo NextFolder in dirInfo)
+            {
+                ForeachFile(NextFolder.FullName, ref  files);
+            }
+        }
+
+        public static bool FilterFile(FileInfo fileItem)
+        {
+            if(fileItem.Name.EndsWith(".meta"))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static HttpResult UploadFile(string LocalPath)
+        {
+            Mac mac = new Mac(Globle.QiNiuAccessKey, Globle.QiNiuSecretKey);
+            // 上传文件名
+            string replacePath = Application.dataPath.Replace("/", "\\") + "\\";
+            string keySuffix = LocalPath.Replace(replacePath, "");
+            string key = Path.Combine(Globle.QiNiuPrefix, Globle.AssetVision, keySuffix).Replace("\\", "/");
+            // 存储空间名
+            string Bucket = Globle.QiNiuBucket;
+            // 设置上传策略
+            PutPolicy putPolicy = new PutPolicy();
+            // 设置要上传的目标空间
+            putPolicy.Scope = Bucket;
+            // 上传策略的过期时间(单位:秒)
+            putPolicy.SetExpires(3600);
+            // 生成上传token
+            string token = Auth.CreateUploadToken(mac, putPolicy.ToJsonString());
+            Config config = new Config();
+            // 设置上传区域
+            config.Zone = Zone.ZONE_CN_East;
+            // 设置 http 或者 https 上传
+            config.UseHttps = true;
+            config.UseCdnDomains = true;
+            config.ChunkSize = ChunkUnit.U512K;
+            // 表单上传
+            FormUploader target = new FormUploader(config);
+            HttpResult result = target.UploadFile(LocalPath, key, token, null);
+            return result;
+        }
+    }
+}
--- Assets/Scripts/Globle.cs
+++ Assets/Scripts/Globle.cs
@@ -5,10 +5,20 @@ namespace DefaultNamespace
     public class Globle : MonoBehaviour
     {
         // 全局变量
-        public static string AssetBundleDir = "AssetBundles";
+        public static string AssetHost = "https://s3.taihuoniao.com/venue"; // 资源文件 域名
+        public static string ServiceHost = "http://render-dev.d3ingo.com/venue"; // 后端业务 域名
+
+        public static string QiNiuPrefix = "unity/venue"; // 七牛资源前缀
+        public static string AssetVision = "2022060101";  // 资源版本
+        public static string QiNiuBucket = "frstatic"; // 七牛Bucket
+        public static string AssetPrefix = AssetHost+QiNiuPrefix+AssetVision;  // 资源请求前缀
+        public static string QiNiuAccessKey = "ERh7qjVSy0v42bQ0fftrFeKYZG39XbzRlaJO4NFy";  //七牛 AccessKey
+        public static string QiNiuSecretKey = "r-NUrKsnRBEwTQxbLONVrK9tPuncXyHmcq4BkSc7"; //七牛 QiNiuSecretKey
+
+
+        public static string AssetBundleDir = "AssetBundles";  // assetBundle 一级目录
         public static string FirstAssetBundle = "firstcontroller.plugin"; // 第一人称 assetBundle 名称
         public static string ThirdAssetBundle = "thirdcontroller.plugin"; // 第三人称 assetBundle 名称
-        public static string AssetHost = "https://cdn1.d3ingo.com/venue"; // 资源文件 域名
-        public static string ServiceHost = "http://render-dev.d3ingo.com/venue"; // 后端业务 域名
+        
     }
 }
