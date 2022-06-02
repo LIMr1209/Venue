1f8d818d032a678a66eb22a08c40136fc6292c9c
LIMr1209 <aaa1058169464@126.com>
2022-06-02 16:18:30 +0800

    pulgins

--- Assets/Scripts/Globle.cs
+++ Assets/Scripts/Globle.cs
@@ -14,6 +14,8 @@ namespace DefaultNamespace
         public static string AssetPrefix = AssetHost+QiNiuPrefix+AssetVision;  // 资源请求前缀
         public static string QiNiuAccessKey = "ERh7qjVSy0v42bQ0fftrFeKYZG39XbzRlaJO4NFy";  //七牛 AccessKey
         public static string QiNiuSecretKey = "r-NUrKsnRBEwTQxbLONVrK9tPuncXyHmcq4BkSc7"; //七牛 QiNiuSecretKey
+        public static string AesIv = "1234567890123456";  // aes 解密 iv
+        public static string AesKey = "12345678901234561234567890123456"; // aes 解密 key
 
 
         public static string AssetBundleDir = "AssetBundles";  // assetBundle 一级目录
