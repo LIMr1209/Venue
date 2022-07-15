9f3ede0c9c8ba5b042377c430f813d1b7f2aecf8
LIMr1209 <aaa1058169464@126.com>
2022-07-15 18:45:16 +0800

    bug

--- Assets/Scripts/Helps/Globle.cs
+++ Assets/Scripts/Helps/Globle.cs
@@ -15,7 +15,7 @@ namespace DefaultNamespace
         public static string AbBucket = "frstatic"; // 项目ab包 上传空间名
         public static string ServiceHost = "http://render-dev.d3ingo.com"; // 后端业务 域名
 
-        public static string QiNiuPrefix = "unity/venue/editor"; // 七牛资源前缀
+        public static string QiNiuPrefix = "unity/venue/dev"; // 七牛资源前缀
 
         public static string AssetVision = "2022071401"; // 资源版本
 
