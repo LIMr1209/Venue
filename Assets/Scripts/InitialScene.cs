928fc0439584984591dded3bf21dda8c9fae6104
LIMr1209 <aaa1058169464@126.com>
2022-07-19 11:23:13 +0800

    角色

--- Assets/Scripts/InitialScene.cs
+++ Assets/Scripts/InitialScene.cs
@@ -127,12 +127,6 @@ namespace DefaultNamespace
                 fps = 60f/_deltaTime;
                 _deltaTime = 0;
             }
-
-            if (Input.GetKeyDown("z"))
-            {
-                FindObjectOfType<JsSend>().JsReloadScene();
-            }
-
             // 可以通过编辑>项目设置>质量找到质量级别列表。您可以添加、删除或编辑这些。
             // int qualityLevel = QualitySettings.GetQualityLevel();
             // QualitySettings.SetQualityLevel (5, true);
